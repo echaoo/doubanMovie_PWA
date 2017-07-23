fd871ad49e0fdb39556163499c37de66e07cf57d
sky-ADMIN <546485299@qq.com>
2017-07-23 13:14:14 +0800

    format code

--- src/pages/Movie/Home.vue
+++ src/pages/Movie/Home.vue
@@ -1,69 +1,74 @@
 <template>
     <div>
-        <movie-list></movie-list>
+        <movie-list :move-list="moveList"></movie-list>
     </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex';
-import MovieList from './MovieList.vue';
+    import {mapActions} from 'vuex';
+    import MovieList from './MovieList.vue';
 
-export default {
-    name: 'home',
-    props: {},
-    methods: {
-        ...mapActions('appShell/appHeader', [
-            'setAppHeader'
-        ]),
-        ...mapActions('appShell/appBottomNavigator', [
-            'showBottomNav',
-            'activateBottomNav'
-        ])
-    },
-    async asyncData() {
-        await new Promise((resolve, reject) => {
-            setTimeout(resolve, 500);
-        });
-    },
-    activated() {
-        this.setAppHeader({
-            show: true,
-            title: '豆瓣电影',
-            showMenu: true,
-            showBack: false,
-            showLogo: true,
-            actions: [
-                {
-                    icon: 'search',
-                    route: '/search'
-                }
-            ],
-            opacity: 1
-        });
-        this.activateBottomNav('home');
-        this.showBottomNav();
-    },
-    components: {
-        MovieList
-    }
-};
+    export default {
+        name: 'home',
+        props: {},
+        data() {
+            return {
+                moveList: []
+            };
+        },
+        methods: {
+            ...mapActions('appShell/appHeader', [
+                'setAppHeader'
+            ]),
+            ...mapActions('appShell/appBottomNavigator', [
+                'showBottomNav',
+                'activateBottomNav'
+            ])
+        },
+        async asyncData() {
+            await new Promise((resolve, reject) => {
+                setTimeout(resolve, 500);
+            });
+        },
+        activated() {
+            this.setAppHeader({
+                show: true,
+                title: '豆瓣电影',
+                showMenu: true,
+                showBack: false,
+                showLogo: true,
+                actions: [
+                    {
+                        icon: 'search',
+                        route: '/search'
+                    }
+                ],
+                opacity: 1
+            });
+            this.activateBottomNav('home');
+            this.showBottomNav();
+        },
+        components: {
+            MovieList
+        }
+    };
 </script>
 
 <style lang="stylus" scoped>
-$text-color := rgba($material-theme.text-color, $material-theme.primary-text-percent)
+    $text-color := rgba($material-theme.text-color, $material-theme.primary-text-percent)
 
-.content
-    width 100%
-    position absolute
-    top 50%
-    left 50%
-    transform translate(-50%, -50%)
+    .content
+        width 100%
+        position absolute
+        top 50%
+        left 50%
+        transform translate(-50%, -50%)
 
-    h2
-        font-size 46px
-        font-weight 500
+        h2
+            font-size 46px
+            font-weight 500
 
-    h2, h4
-        color $text-color
+        h2, h4
+            color $text-color
 
 </style>
